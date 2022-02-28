b3a624d54f4fcc40cc59e325d3e7af6be67a7133
Nj <Nj.linqc@gmail.com>
2022-02-27 20:48:02 -0500

    added command and event handlers

--- index.js
+++ index.js
@@ -8,42 +8,12 @@ const client = new Discord.Client({
     ]}
 );
 
-const fs = require('fs');
-
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
-// ['command_handler', 'event_handler'].forEach(handler => {
-//     require(`./handlers/${handler}`)
-// })
-
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles){
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-}
-
-const prefix = '.';
-
-client.once('ready', () => {
-    console.log('Sad Pepe is online...');
-});
-
-client.on('messageCreate', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(' ');
-    const command = args.shift().toLowerCase();
-
-    for (const key of client.commands.keys()){
-        if (command == key){
-            client.commands.get(key).execute(message,args);
-            break;
-        }
-    }
-
-});
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client)
+})
 
 const token = '';
 
