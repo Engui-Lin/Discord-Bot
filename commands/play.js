a1077aa762dec8138823029d958878fe94d81ecb
Nj <Nj.linqc@gmail.com>
2022-03-05 23:46:09 -0500

    changed play.js

--- commands/play.js
+++ commands/play.js
@@ -24,26 +24,46 @@ module.exports = {
             return (videoResults.videos.length > 1) ? videoResults.videos[0] : null;
         }
         
-        // const validURL = (str) => {
-        //     var regex = /(http|https):\/\/(w+:{0,1}\w*)?
-        // }
-        
-        
+        const validURL = (str) =>{
+            var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+            if(!regex.test(str)){
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        const connection = joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: message.guild.id,
+            adapterCreator: message.guild.voiceAdapterCreator,
+        });
+
+        const audioPlayer = createAudioPlayer({
+            behaviors : {
+                noSubscriber: NoSubscriberBehavior.Pause,
+            },
+        });
+
+        if (validURL(args[0])){
+            
+            const stream = await ytdl(args[0], {filter: 'audioonly'});
+
+            const resource = createAudioResource(stream);
+
+            audioPlayer.play(resource);
+            connection.subscribe(audioPlayer);
+            
+            const newEmbed = new MessageEmbed()
+            .setDescription(`Now playing: [***${args[0]}***](${args[0]})`)
+            message.channel.send({embeds: [newEmbed]});
+
+            return;
+        }
+    
         const video = await videoFinder(args.join(' '));
 
         if (video) {
-
-            const connection = joinVoiceChannel({
-                channelId: voiceChannel.id,
-                guildId: message.guild.id,
-                adapterCreator: message.guild.voiceAdapterCreator,
-            });
-    
-            const audioPlayer = createAudioPlayer({
-                behaviors : {
-                    noSubscriber: NoSubscriberBehavior.Pause,
-                },
-            });
             
             const stream = await ytdl(video.url, {filter: 'audioonly'});
 
@@ -56,25 +76,17 @@ module.exports = {
             .setDescription(`Now playing: [***${video.title}***](${video.url})`)
             message.channel.send({embeds: [newEmbed]});
             
-            // connection.on('stateChange', (oldState, newState) => {
-            //     console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
-            //     });
-            audioPlayer.on('stateChange', (oldState, newState) => {
-                console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
-                });
-
-            audioPlayer.on('idle', () => {
-                setTimeout(()=>{
-                    if (audioPlayer.state.status == 'idle'){
-                        console.log('idle starts........')
-                        connection.destroy();
-                        console.log('Disconnected from voice channel...');
-                    }
-                    }, 60000);
-                });
         }
         else{
             message.channel.send('No results found...');
         } 
+
+        // connection.on('stateChange', (oldState, newState) => {
+        //     console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
+        //     });
+        audioPlayer.on('stateChange', (oldState, newState) => {
+            console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
+            });
+
     }
 }
